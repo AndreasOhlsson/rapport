a6a1cba232d4a44f2e59916aef15539ef04a93ad
Andreas Ohlsson <andreasxohlsson@gmail.com>
2018-04-21 14:51:06 +0200

    fixed bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ApplicationRecord
   
   has_and_belongs_to_many :tickets
 
-  validates :first_name, presence: true, length: { maximum: 50 }
-  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email, presence: true, length: { maximum: 255 },
