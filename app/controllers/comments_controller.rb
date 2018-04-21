811aef435abee74e3ae994265b558b1dd4245c68
Andreas Ohlsson <andreasxohlsson@gmail.com>
2018-04-21 21:47:17 +0200

    added mailer to notify on comments at tickets

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,14 @@ class CommentsController < ApplicationController
       flash[:success] = 'Ny kommentar skapad'
       if (!user_signed_in?)
         @ticket.update(updated_at: Time.now)
+        @ticket.comments.each do |c|
+          if !c.user_id.nil?
+            user = User.find(c.user_id)
+            token = @ticket.token
+            # send email
+            UserMailer.notify_new_comment(user, token).deliver
+          end
+        end
       end
       redirect_to ticket_url(@ticket.token)
     else
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+
+  # new comment has been added to a ticket by anon
+  def notify_new_comment(user, token)
+    @ticket = Ticket.find_by(token)
+    @user = user
+    mail(to: @user.email, subject: "Ny kommentar av anonym på #{@ticket.title}"" )
+  end
+
 end
